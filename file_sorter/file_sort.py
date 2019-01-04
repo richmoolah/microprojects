2299d67e822982755841878b4ac27f791bc2e19f
Rxcn5 <richardxcn5@gmail.com>
2019-01-03 22:31:44 -0800

    Update file_sort.py

--- file_sorter/file_sort.py
+++ file_sorter/file_sort.py
@@ -1,5 +1,7 @@
 import shutil, os, sys
 
+#For Ubuntu
+
 def get_subdir():
    return [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
 
@@ -14,15 +16,15 @@ def sort_files():
             print(foldername)
             if foldername in filename:
                 print('success')
-                shutil.move(os.getcwd()+'\\'+filename, os.getcwd()+'\\'+foldername)
+                shutil.move(os.getcwd()+'/'+filename, os.getcwd()+'/'+foldername)
                 find = True
                 break
         if find == False:
             user_input = input(filename+' does not match with any existing folder. Do you want to create a new folder? (y/n/end)\n')
             if user_input == 'y':
                 user_folder = input('Name your new folder: ')
-                os.makedirs(os.getcwd()+'\\'+user_folder)
-                shutil.move(os.getcwd()+'\\'+filename, os.getcwd()+'\\'+user_folder)
+                os.makedirs(os.getcwd()+'/'+user_folder)
+                shutil.move(os.getcwd()+'/'+filename, os.getcwd()+'/'+user_folder)
             elif user_input == 'n':
                 pass
             elif user_input == 'end':
