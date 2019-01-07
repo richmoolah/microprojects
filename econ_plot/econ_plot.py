a99495a92f4a425a76636e14909514971c347696
richardxu204 <46305856+richardxu204@users.noreply.github.com>
2019-01-06 20:39:03 -0500

    Update econ_plot.py
    CKAN api request

--- econ_plot/econ_plot.py
+++ econ_plot/econ_plot.py
@@ -1,1 +1,12 @@
 import matplotlib.pyplot
+import urllib, urllib2
+
+response = urllib2.urlopen('http://demo.ckan.org/api/3/action/group_list', data_string)
+assert response.code == 200
+
+response_dict = json.loads(response.read())
+
+assert response_dict['success'] is True
+result = response_dict['result']
+
+print(result)
